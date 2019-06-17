4cf8c0bb8182b935c3e88565a472477b2b13c4ad
Andriy <andriy.andriyan1@gmail.com>
2019-06-17 17:13:52 +0700

    Change site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Devghuraba Teknologi`,
+    description: `Jasa Pembuatan Website, Aplikasi Android & Aplikasi iOS.`,
+    author: `@devghuraba`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
