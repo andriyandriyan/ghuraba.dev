141a79da7248b40553073a56ff2f9178bb0f778b
Andriy <andriy.andriyan1@gmail.com>
2019-06-18 13:53:50 +0700

    tidy up the code and add logo on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,89 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
-const Header = ({ siteTitle }) => (
-	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-	<div class="container">
-	  <a class="navbar-brand" href="index.html">{siteTitle}</a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="oi oi-menu"></span> Menu
-	  </button>
+const Header = ({ siteTitle }) => {
+	const { placeholderImage } = useStaticQuery(
+		graphql`
+			query {
+				placeholderImage: file(relativePath: { eq: "logo.png" }) {
+					childImageSharp {
+						fluid(maxWidth: 400) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+			}
+		`
+	);
 
-	  <div class="collapse navbar-collapse" id="ftco-nav">
-		<ul class="navbar-nav ml-auto">
-		  <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-		  <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-		  <li class="nav-item"><a href="domain.html" class="nav-link">Domain</a></li>
-		  <li class="nav-item"><a class="nav-link" href="hosting.html">Hosting</a></li>
-		  <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-		  <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-		  <li class="nav-item cta"><a href="contact.html" class="nav-link"><span>Get started</span></a></li>
-		</ul>
-	  </div>
-	</div>
-  </nav>
-)
+	return (
+		<nav
+			className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light"
+			id="ftco-navbar"
+		>
+			<div className="container">
+				<Link className="navbar-brand" to="/">
+					<Img
+						alt={siteTitle}
+						title={siteTitle}
+						fluid={placeholderImage.childImageSharp.fluid}
+					/>
+				</Link>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#ftco-nav"
+					aria-controls="ftco-nav"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="oi oi-menu"></span> Menu
+				</button>
+
+				<div className="collapse navbar-collapse" id="ftco-nav">
+					<ul className="navbar-nav ml-auto">
+						<li className="nav-item">
+							<Link activeClassName="active" className="nav-link" to="/">
+								Home
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link activeClassName="active" className="nav-link" to="#">
+								About
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link activeClassName="active" className="nav-link" to="#">
+								Blog
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link activeClassName="active" className="nav-link" to="#">
+								Contact
+							</Link>
+						</li>
+						<li className="nav-item cta">
+							<Link className="nav-link" to="#">
+								<span>Get started</span>
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+	siteTitle: PropTypes.string,
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+	siteTitle: ``,
+};
 
-export default Header
+export default Header;
