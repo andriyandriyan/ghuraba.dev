9ac0f2f07f60bc97961b351e2553dfa4f1c4544e
Andriy <andriy.andriyan1@gmail.com>
2019-06-18 15:17:14 +0700

    Add dinamic class for fixed navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
@@ -19,11 +19,36 @@ const Header = ({ siteTitle }) => {
 		`
 	);
 
+	let timeOut = 0;
+	const baseNavbarClass =
+		'navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light';
+	const [navbarClass, setNavbarClass] = useState('');
+
+	useEffect(() => {
+		window.onscroll = () => {
+			const st = document.documentElement.scrollTop;
+			let navClass;
+			if (st > 150) {
+				navClass = 'sleep scrolled awake';
+			} else if (st < 150) {
+				navClass = 'sleep scrolled ';
+				if (st > 30) {
+					navClass = 'sleep awake';
+				} else if (st < 30) {
+					navClass = 'sleep';
+				}
+			}
+
+			clearTimeout(timeOut);
+			timeOut = setTimeout(() => {
+				setNavbarClass(navClass);
+				console.log(navClass);
+			}, 200);
+		};
+	}, []);
+
 	return (
-		<nav
-			className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light"
-			id="ftco-navbar"
-		>
+		<nav className={`${baseNavbarClass} ${navbarClass}`} id="ftco-navbar">
 			<div className="container">
 				<Link className="navbar-brand" to="/">
 					<Img
