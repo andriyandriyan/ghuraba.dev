0ec563fdca9990484f582aa8f81907dba69cb349
Andriy <andriy.andriyan1@gmail.com>
2019-06-19 14:01:03 +0700

    Change image hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,24 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 import '../styles/index.scss';
-import image from '../images/undraw_pair_programming_njlp.svg';
+import image from '../images/undraw_startup_life_2du2.svg';
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" />
-		<div class="hero-wrap js-fullheight">
-			<div class="overlay"></div>
-			<div class="container-fluid px-0">
-				<div class="row d-md-flex no-gutters slider-text align-items-center js-fullheight justify-content-end">
+		<div className="hero-wrap js-fullheight">
+			<div className="overlay"></div>
+			<div className="container-fluid px-0">
+				<div className="row d-md-flex no-gutters slider-text align-items-center js-fullheight justify-content-end">
 					<img
-						class="one-third js-fullheight align-self-end order-md-last img-fluid"
+						className="one-third js-fullheight align-self-end order-md-last img-fluid"
 						src={image}
 						alt=""
 					/>
-					<div class="one-forth d-flex align-items-center ftco-animate js-fullheight">
-						<div class="text mt-5">
-							<span class="subheading">Isometric Hosting</span>
-							<h1 class="mb-3">
+					<div className="one-forth d-flex align-items-center ftco-animate js-fullheight">
+						<div className="text mt-5">
+							<span className="subheading">Isometric Hosting</span>
+							<h1 className="mb-3">
 								<span>Design,</span> <span>Development,</span>{' '}
 								<span>Hosting</span>
 							</h1>
@@ -31,7 +31,7 @@ const IndexPage = () => (
 								Vokalia and Consonantia, there live the blind texts.
 							</p>
 							<p>
-								<a href="#" class="btn btn-primary px-4 py-3">
+								<a href="#" className="btn btn-primary px-4 py-3">
 									Get in touch
 								</a>
 							</p>
