4dfc3d9b60a06fe61171115352270fa45baec754
andriyandriyan <andriy.andriyan1@gmail.com>
2019-06-28 08:11:30 +0700

    import footer to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
+import Footer from './footer';
 
 const Layout = ({ children }) => (
 	<StaticQuery
@@ -26,11 +27,7 @@ const Layout = ({ children }) => (
 			<>
 				<Header siteTitle={data.site.siteMetadata.title} />
 				{children}
-				<footer>
-					© {new Date().getFullYear()}, Built with
-					{` `}
-					<a href="https://www.gatsbyjs.org">Gatsby</a>
-				</footer>
+				<Footer />
 			</>
 		)}
 	/>
